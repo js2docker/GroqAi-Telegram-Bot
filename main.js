91fce2700bd39a5865cd60e608ab48cabec32985
js2docker <44265853+js2docker@users.noreply.github.com>
2024-05-28 22:21:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const Groq = require("groq-sdk");
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your Telegram bot token and Groq API key
-const TELEGRAM_TOKEN = 'YOUR_TELEGRAM_TOKEN';
-const GROQ_API_KEY = 'YOUR_GROP_API_KEY';
+const TELEGRAM_TOKEN = '5815567565:AAGdRjgvkSVv_aX90blgi1X8WNmpnJRu9ro';
+const GROQ_API_KEY = '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';
 
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 const groq = new Groq({ apiKey: GROQ_API_KEY });
